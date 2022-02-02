3410a676ca7484ceda662c7299bc8a498bed47e8
Sorcha <shdouglas003@gmail.com>
2022-02-01 21:54:51 -0600

    19-1-4 and 19-1-5 exercises

--- 19-Week/19-1/19-1-5/js/index.js
+++ 19-Week/19-1/19-1-5/js/index.js
@@ -1,1 +1,6 @@
 // Add Event Listener Below
+$('#toggle').on('click', function() {
+    $('.more-list').slideToggle();
+    // $('.down-arrow').append("<img src='img/dog-eating-apple.jpg'>");
+});
+
