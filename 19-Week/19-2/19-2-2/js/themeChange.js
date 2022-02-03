317ee5796f95585ee5a1ac2db152ec190f741bf6
Sorcha <shdouglas003@gmail.com>
2022-02-03 15:59:53 -0600

    19-2-2 complete

--- 19-Week/19-2/19-2-2/js/themeChange.js
+++ 19-Week/19-2/19-2-2/js/themeChange.js
@@ -18,3 +18,17 @@ function w3_open() {
     captionText.innerHTML = element.alt;
   }
   
+$('.themeChange').on("click", function () {
+  
+  if ($(this).hasClass("regular")) {
+    $('#theme').attr("href", "css/blank.css");
+
+  } else if ($(this).hasClass("blue")) {
+    $('#theme').attr("href", "css/blue.css");
+
+  } else if ($(this).hasClass("dark")) {
+    $('#theme').attr("href", "css/dark.css");
+
+  }
+
+});
